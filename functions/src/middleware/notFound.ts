209baf083331be20f919836381aa232b3f743a0e
castrokevyn <kevyn.castro@zipeli.com>
2021-09-11 00:36:59 +0800

    parameter alignment

--- functions/src/middleware/notFound.ts
+++ functions/src/middleware/notFound.ts
@@ -1,4 +1,11 @@
-export const errorHandler = (err: { message: any; stack: any; }, req: any, res: { statusCode: number; status: (arg0: any) => void; json: (arg0: { message: any; stack: any; }) => void; }, next: any) => {
+export const errorHandler = (
+    err: { message: any; stack: any; }, 
+    req: any, 
+    res: { 
+        statusCode: number; 
+        status: (arg0: any) => void; 
+        json: (arg0: { message: any; stack: any; }
+) => void; }, next: any) => {
     const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
     res.status(statusCode);
     res.json({
